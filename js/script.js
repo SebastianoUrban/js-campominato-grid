e094bf5952f5938dfe34e5ab54509c6a1e102037
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-11-30 16:16:08 +0100

    added reaction to click of square

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 
+//I take the elements of the DOM
 const gridElem = document.getElementById('grid');
+const playBtn = document.getElementById('play');
 
-for (let i=0; i<100; i++) {
+function createSquareElem (index) {
     const elem = document.createElement('div');
     elem.classList.add('square');
-    elem.innerText = i+1;
-    gridElem.appendChild(elem);
-}
+    elem.innerText = index+1;
+    
+    elem.addEventListener('click', function () {
+        console.log(index+1);
+        elem.classList.toggle('clicked');
+        });
+    
+    return elem;
+};
+
+
+
+//function for fill the 10x10 grid with div.squere
+function fillGrid(gridElem) {
+    for (let i=0; i<100; i++) {
+        squareElem = createSquareElem(i);
+        gridElem.appendChild(squareElem);
+    }
+};
+
+fillGrid(gridElem);
