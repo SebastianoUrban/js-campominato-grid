49c573d07b82e3791fadaab993652005840a7e6a
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-11-30 17:15:34 +0100

    added play button function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 
 //I take the elements of the DOM
 const gridElem = document.getElementById('grid');
-const playBtn = document.getElementById('play');
+const playBtn = document.getElementById('btn-play');
 
 
 // fanction for creation div.square with inside p
 function createSquareElem (index) {
-
+    //creation the p element with inside number of cell
     const pElem = document.createElement('p');
     pElem.innerText = index+1;
-
+    //creation the div.square element and inserting the p element
     const divElem = document.createElement('div');
     divElem.classList.add('square');
     divElem.appendChild(pElem);
-    
+    //add behavior to the cell
     divElem.addEventListener('click', function () {
         console.log(index+1);
         divElem.classList.toggle('clicked');
         });
-    
+    //return the div.squared element
     return divElem;
 };
 
@@ -26,10 +26,16 @@ function createSquareElem (index) {
 
 //function for fill the 10x10 grid with div.squere
 function fillGrid(gridElem) {
+    gridElem.innerHTML = '';
     for (let i=0; i<100; i++) {
         squareElem = createSquareElem(i);
         gridElem.appendChild(squareElem);
     }
 };
 
-fillGrid(gridElem);
+
+
+
+playBtn.addEventListener('click', function(){
+    fillGrid(gridElem);
+});
